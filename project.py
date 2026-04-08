2b8a35401262add581912fcdf21e34cc30da6db1
Steentoft <chrsteentoft@gmail.com>
2026-04-08 10:53:18 +0200

    inital commit

--- project.py
+++ project.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -40,7 +42,32 @@ plt.show()
 # def obj_func ():
     # path_length = 
 
+def path_length(x):
+    sum = 0
+    for i in range(x-1):
+        sum += abs(x[i+1]-x[i])**2
+    return sum
 
+def favour_smoothness(x):
+    sum = 0
+    for i in range(x-1):
+        sum += abs(x[i+1]-2*x[i]+x[x-1])**2
+    return sum
+
+def avoid_obstacles(x):
+    sum = 0
+    for i in range(x):
+        sum += penalty_1(x[i])
+    return sum
+
+
+
+def penalty_1(x):
+    if True:
+        1/(x)
+
+def penalty_2(x):
+    pass
 
 
 # Adding something to penalize big spacing between points. Potentially recreating the line
