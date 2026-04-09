48c313fdb4406e437b33119e592347ee65454caf
Krumme2004 <Daniel@flemmingsfoto.dk>
2026-04-09 11:31:47 +0200

    pushy

--- asg1/src/case1/objective_function.py
+++ asg1/src/case1/objective_function.py
@@ -14,34 +14,38 @@ def f_L(x):
     sum = 0.0
     for i in range(len(x)-1):
         sum += abs(x[i+1]-x[i])**2
-    a
     return an.sum(sum)
 
-f_L_grad = grad(f_L)
 
 def gradient_f_L(x):
-    return f_L_grad(x)
-
-print(gradient_f_L(x_init_line))
-
+    return grad(f_L)(x)
 
 
 def f_S(x):
     sum = 0.0
     for i in range(len(x)-1):
         sum += abs(x[i+1]-2*x[i]+x[i-1])**2
-    return sum
+    return an.sum(sum)
 
+def gradient_f_S(x):
+    return grad(f_S)(x)
+
+    
 def f_O(x):
     sum = 0.0
     for i in range(len(x)):
         sum += penalty_2(x[i])
-    return sum
+    return an.sum(sum)
+
+def gradient_f_O(x):
+    return grad(f_O)(x)
+
+print(gradient_f_O(x_init_line))
 
 def penalty_2(x, obstacles, alpha=1):
     penalty = 0.0
     for i in range(len(obstacles)):
-        penalty += np.exp(-alpha*(circular_obstacle(x, obstacles[i])**2-obstacles[i][1]**2))
+        penalty += an.exp(-alpha*(circular_obstacle(x, obstacles[i])**2-obstacles[i][1]**2))
     return penalty
 
 def circular_obstacle(x, obstacle):
