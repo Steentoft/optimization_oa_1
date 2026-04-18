91123c719373f62f3149179ee9f5cea85b613a86
Krumme2004 <Daniel@flemmingsfoto.dk>
2026-04-18 14:30:24 +0200

    push is working

--- asg1/src/case1/adamw.py
+++ asg1/src/case1/adamw.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def adamw(x, fun, args):
-    v = np.zeros_like(x).flatten()
-    s = np.zeros_like(x).flatten()
+    v = np.zeros_like(x)
+    s = np.zeros_like(x)
     t = 0
     
     iterations, lr, gamma_v, gamma_s, epsilon, weight_decay = args[0], args[1], args[2], args[3], args[4], args[5]
@@ -13,7 +13,7 @@ def adamw(x, fun, args):
     best_line = np.copy(x)
 
     for i in range(iterations):
-        adam_gradient = fun(this_x)[1].flatten()
+        adam_gradient = fun(this_x)[1]
 
         t += 1
 
@@ -23,10 +23,10 @@ def adamw(x, fun, args):
         v_hat = v / (1 - gamma_v**t)
         s_hat = s / (1 - gamma_s**t)
 
-        decay = lr * weight_decay * x[1:-1]
+        decay = lr * weight_decay * this_x[1:-1]
         next_x = (1.0 / (epsilon + np.sqrt(s_hat[1:-1]))) * v_hat[1:-1]
 
-        this_x[1:-1] = this_x[1:-1].flatten() + next_x - decay
+        this_x[1:-1] = this_x[1:-1] + next_x - decay
 
         if fun(this_x)[0] < min_adam_objective_value:
             min_adam_objective_value = fun(this_x)[0]
