1531cb8c6c0b55c4de7ed352bb1627e26db581e3
Steentoft <chrsteentoft@gmail.com>
2026-04-18 12:55:57 +0200

    normalized the code and added single run point from main

---
+++ asg1/src/case1/main.py
@@ -0,0 +1,80 @@
+from objective_function import objective_function
+from gradient_descent import gradient_descent
+from optimizer import CG_optimizer
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+########## Task 1 ##########
+x_start = np.array([0.0, 0.0])
+x_end = np.array([7.0, 10.0])
+
+# (Point, Radius, Color)
+obstacles = [
+    (np.array([2, 4]), 1.3, 'blue'),
+    (np.array([5, 7]), 1.0, 'orange')
+]
+n_points = 20
+
+x_init_line = np.linspace(x_start, x_end, n_points)
+
+iterations = 20
+
+### Plotting
+fig, ax = plt.subplots(figsize=(6, 6))
+ax.plot(x_init_line[:, 0], x_init_line[:, 1], marker='.', label="Initial Path")
+
+for j in range(len(obstacles)):
+    ax.add_patch(plt.Circle(obstacles[j][0], obstacles[j][1], color=obstacles[j][2]))
+
+def plot_inner_flat_line(x):
+    new_line = x_init_line.copy()
+    x = x.reshape((-1, 2))
+    new_line[1:-1] = x
+    return new_line
+
+obj_fun = lambda x: objective_function(x, x_init_line, obstacles)
+
+functions = [
+    gradient_descent,
+]
+
+optimizers = [
+    CG_optimizer
+]
+
+def main():
+
+    for function in functions:
+        best_line = x_init_line.copy()[1:-1].flatten()
+
+        for iteration in range(iterations):
+            next_line = best_line.copy()
+
+            for i in range(iterations):
+                next_line = function(next_line, obj_fun)
+
+            if obj_fun(next_line)[0] < obj_fun(best_line)[0]:
+                best_line = next_line.copy()
+
+        best_line = plot_inner_flat_line(best_line)
+
+        ax.plot(best_line[:, 0], best_line[:, 1], marker='.', label=f"Best Path")
+
+    for optimizer in optimizers:
+        new_line = x_init_line[1:-1].flatten()
+
+        res = optimizer(new_line, obj_fun, iterations)
+
+        rebuilt_x = plot_inner_flat_line(res.x)
+        ax.plot(rebuilt_x[:, 0], rebuilt_x[:, 1], marker='.', label=f"Best Path")
+
+
+    ax.set_xlim(-0.5, 11)
+    ax.set_ylim(-0.5, 11)
+    ax.legend()
+
+    plt.show()
+
+if __name__=="__main__":
+    main()
--- asg1/src/case1/optimizer.py
+++ asg1/src/case1/optimizer.py
@@ -1,70 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
-from objective_function import *
+def CG_optimizer(x, fun, iterations=100):
+    new_x = x.copy()
 
-########## Task 1 ##########
-x_start = np.array([0.0, 0.0])
-x_end = np.array([7.0, 10.0])
+    res = minimize(fun, new_x, method='CG', tol=0.001, jac=True, options={'maxiter': iterations})
 
-# (Point, Radius, Color)
-obstacles = [
-    (np.array([2, 4]), 1.3, 'blue'),
-    (np.array([5, 7]), 1.0, 'orange')
-]
-n_points = 20
-
-x_init_line = np.linspace(x_start, x_end, n_points)
-
-### Plotting
-fig, ax = plt.subplots(figsize=(6, 6))
-ax.plot(x_init_line[:, 0], x_init_line[:, 1], marker='.', label="Initial Path")
-
-for j in range(len(obstacles)):
-    ax.add_patch(plt.Circle(obstacles[j][0], obstacles[j][1], color=obstacles[j][2]))
-
-def plot_inner_flat_line(x):
-    new_line = x_init_line.copy()
-    x = x.reshape((-1, 2))
-    new_line[1:-1] = x
-    return new_line
-
-########## Task 4 ##########
-fun = lambda x: objective_function(x, x_init_line, obstacles)
-
-### Optimizer ###
-inner_x_init = x_init_line[1:-1].flatten()
-res = minimize(fun, inner_x_init, method='CG', tol=0.001, jac=True, options={'maxiter': 100})
-
-rebuilt_x = plot_inner_flat_line(res.x)
-ax.plot(rebuilt_x[:, 0], rebuilt_x[:, 1], marker='.', label=f"Optimizer {res.fun}]")
-
-best_line = x_init_line.copy()
-
-### Gradient Descent ###
-best_inner_line = x_init_line.copy()[1:-1].flatten()
-inner_new_line = np.copy(best_inner_line)
-
-epochs = 100
-learning_rate = 0.001
-
-for e in range(epochs):
-    inner_new_line = best_inner_line
-
-    new_objective_value, new_gradient_array = fun(inner_new_line)
-
-    for j in range(len(inner_new_line)):
-        inner_new_line[j] = (inner_new_line[j] - (learning_rate * new_gradient_array[j])) #Update step
-
-    if fun(inner_new_line)[0] < fun(best_inner_line)[0]:
-        best_inner_line = inner_new_line
-
-best_inner_line = plot_inner_flat_line(best_inner_line)
-ax.plot(best_inner_line[:, 0], best_inner_line[:, 1], marker='.', label=f"Best Path {fun(best_inner_line[1:-1].flatten())[0]}")
-
-ax.set_xlim(-0.5, 11)
-ax.set_ylim(-0.5, 11)
-ax.legend()
-
-plt.show()
+    return res
