00cb1626885afcc7a2b287cae95bd3239f25ea18
Steentoft <chrsteentoft@gmail.com>
2026-04-18 15:49:32 +0200

    Update optimizer.py

--- asg1/src/case1/optimizer.py
+++ asg1/src/case1/optimizer.py
@@ -3,6 +3,6 @@ from scipy.optimize import minimize
 def CG_optimizer(x, fun, iterations=100):
     new_x = x.copy()
 
-    res = minimize(fun, new_x, method='CG', tol=0.01, jac=True, options={'maxiter': iterations})
+    res = minimize(fun, new_x, method='CG', tol=1e-6, jac=True, options={'maxiter': iterations})
 
     return res
